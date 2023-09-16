20342cb737d3c1290b2c972af03761c533b2f046
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-09-16 05:51:28 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Companies from "./components/Companies";
+import Popular_services from "./components/Popular_services";
+import Best_part from "./components/Best_part";
+import Got from "./components/Got";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Banner />
+      <Companies />
+      <Popular_services />
+      <Best_part />
+      <Got />
     </div>
   );
 }
